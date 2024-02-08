35131c541654d4f02ba63bd47ab72706ec8a4d37
Allen Saji <allensaji04@gmail.com>
2024-02-08 15:23:57 +0530

    feat: add collapsable side ar

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,19 +1,49 @@
 import React, { useState } from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { RiArrowLeftDoubleLine, RiArrowRightDoubleFill } from "react-icons/ri";
 import DragAndDropInput from "@/components/ui/DragAndDropInput";
 
-export default function Home() {
-  const [activeTab, setActiveTab] = useState<string>("upload");
+type TabType = "upload" | "real-time";
 
-  const handleTabClick = (value: string) => {
+export default function Home(): JSX.Element {
+  const [activeTab, setActiveTab] = useState<TabType>("upload");
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState<boolean>(false);
+
+  const handleTabClick = (value: TabType) => {
     setActiveTab(value);
   };
 
+  const toggleSidebar = () => {
+    setIsSidebarCollapsed(!isSidebarCollapsed);
+  };
+
   return (
-    <div className="flex w-screen">
-      <div className="flex-none bg-customBlack w-40 h-screen"></div>
-      <div className="flex-col flex h-full w-screen mt-10 items-center justify-center">
-        <div className="flex w-full justify-center ">
+    <div className="relative flex w-screen">
+      <div
+        className={`flex-none bg-customBlack ${
+          isSidebarCollapsed ? "w-0" : "w-40"
+        } h-screen overflow-hidden transition-all`}
+      >
+        {/* Sidebar content goes here */}
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={toggleSidebar}
+          className={`p-2 bg-gray-600 text-green  h-20 bg-transparent`}
+        >
+          {isSidebarCollapsed ? (
+            <RiArrowRightDoubleFill className="text-[#67CCD6]" />
+          ) : (
+            <RiArrowLeftDoubleLine className="text-[#67CCD6]" />
+          )}
+        </button>
+      </div>
+      <div
+        className={
+          "flex-col flex h-full w-full mt-10 items-center justify-center"
+        }
+      >
+        <div className="flex w-full justify-center">
           <Tabs defaultValue="upload">
             <TabsList>
               <TabsTrigger
