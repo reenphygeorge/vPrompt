c07752d14e6061d6232bd4abb3375043bd31275a
reenphygeorge <reenphygeorge@gmail.com>
2024-04-02 10:47:10 +0530

    feat(core): redis memory limit

--- api/utils/cache.py
+++ api/utils/cache.py
@@ -6,12 +6,16 @@ from os import environ
 load_dotenv()
 redis_host = environ["REDIS_HOST"]
 redis_port = environ["REDIS_PORT"]
+redis_memory_limit = environ["REDIS_MEMORY_LIMIT"]
 
 redis_client = Redis(
     host=redis_host,
     port=redis_port,
     db=0,
 )
+
+memory_limit = 1024 * 1024 * int(redis_memory_limit)
+response = redis_client.execute_command("CONFIG", "SET", "maxmemory", memory_limit)
 
 def add_cache(key: str, data, json: bool):
     if json:
